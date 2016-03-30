dbb0e4fde2123ebeeae34ac96ec3aae4ff167811
i12orore <i12orore@uco.es>
2016-03-30 18:29:30 +0200

    Completado Donante, funciona todo correctamente

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
 # Practica2ED
 
 Primera Parte
-
-Falta
-  - Definir las correcciones de errores para las variables, es decir,que si meto un valor que no pueda ser que me de un error.
-  - Asegurarse que el <= esta correcto
+- Completa
 
 Segunda Parte
-
-Falta
-  - Limpiar buffer para que me lea bien los donantes
-  - Crear el menu para que:
-    Lea un donante
-    Modifique un donante
-    Escriba un donante
-    Compare lexicograficamente los donantes
-    Fin del programa
+- Completa
 
 Tercera Parte
-
-Aun queda.
--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@ using namespace std;
 using namespace ed;
 int main(){
   int opcion=0;
-  Donante d,d2,d3;
-  bool iguales;
-
+  Donante d,d3;
+  bool iguales,menor;
+  Donante d2(d);
   do{
     cout<<"\nMenu\n";
     cout<<"\n----------------------------\n";
     std::cout << "1.- Leer donante" << std::endl;
     std::cout << "2.- Mostrar donante" << std::endl;
     std::cout << "3.- Modificar los datos de un donante" << std::endl;
+    std::cout << "4.- Comparar el donante introducido en la opción 1 con el modificado en la opción 3" << std::endl;
+    std::cout << "5.- Comprobar que el donante introducido en la opción 1 esta por delante del introducido en la opción 3" << std::endl;
+    std::cout << "6.- Salir del programa" << std::endl;
     std::cin >> opcion;
     switch (opcion) {
       case 1:
@@ -33,6 +36,20 @@ int main(){
           std::cout << "Nuevos datos del donante" << std::endl;
           d3.escribirDonante();
           break;
+      case 4:
+
+          iguales=(d==d3);
+          if(iguales)
+            std::cout << "Funciona el ==" << std::endl;
+          break;
+      case 5:
+          menor=(d<=d3);
+          if(menor)
+            std::cout << "Fnciona el <=" << std::endl;
+          break;
+      case 6:
+          return 0;
+          break;
       default:
           std::cout << "Error: Introduce correctamente una de las opciones designadas" << std::endl;
           break;
@@ -41,21 +58,6 @@ int main(){
 
 
 
-  }while(opcion!=5);
-
-
-
-  d2.leerDonante();
-  iguales=(d2==d);
-  if(iguales)
-    std::cout << "Funciona el ==" << std::endl;
-  d.escribirDonante();
-  cout<<endl;
-  d2.escribirDonante();
-  cout<<endl;
-  d3.escribirDonante();
-  cout<<endl;
-  Donante d4(d2);
-  d4.escribirDonante();
+  }while(opcion!=6);
   return 0;
 }
